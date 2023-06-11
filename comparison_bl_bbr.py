9e28b99a455ccf1f2f11be5d46980cb588278a42
Crowkas <nicolas019724@gmail.com>
2023-06-11 15:18:49 -0400

    Changes were made to make the code more efficient and avoid redundancies.

--- comparison_bl_bbr.py
+++ comparison_bl_bbr.py
@@ -41,19 +41,35 @@ if __name__ == '__main__':
     list_size = int(input('De que tamaño sera la lista?\n'))
     target = int(input('Que numero quieres encontrar?\n'))
 
-    list_l = [random.randint(0, 1000000) for i in range(list_size)]
-    list_b_r = sorted([random.randint(0, 1000000) for i in range(list_size)])
-
-    counter_iterations = 0
-    found_l, counter_iterations_l = busqueda_lineal(list_l, target)
-    found_b_r, counter_iterations_b_r = busqueda_binaria_r(list_b_r, 0, len(list_b_r), target, counter_iterations)
-
-    print(list_l) 
-    print(f'\n {list_b_r}')
+    list_l = [random.randint(0, 10) for i in range(list_size)]
+    list_b_r = sorted([random.randint(0, 10) for i in range(list_size)])
     
-    print(f'\nUsando Busqueda Lineal\nEl elemento {target} {"esta" if found_l else "no esta"} en la lista')
-    print(f'Se realizaron {counter_iterations_l} pasos')
+    if target in list_l and target in list_b_r:
 
-    print(f'\nUsando Busqueda Binaria Recursiva\nEl elemento {target} {"esta" if found_b_r else "no esta"} en la lista')
-    print(f'Se realizaron {counter_iterations_b_r} pasos')
+        counter_iterations = 0
+        found_l, counter_iterations_l = busqueda_lineal(list_l, target)
+        found_b_r, counter_iterations_b_r = busqueda_binaria_r(list_b_r, 0, len(list_b_r), target, counter_iterations)
+
+        print(f'{list_l}\n {list_b_r}')
+        
+        print(f'\nUsando Busqueda Lineal\nEl elemento {target} {"esta" if found_l else "no esta"} en la lista')
+        print(f'Se realizaron {counter_iterations_l} pasos')
+
+        print(f'\nUsando Busqueda Binaria Recursiva\nEl elemento {target} {"esta" if found_b_r else "no esta"} en la lista')
+        print(f'Se realizaron {counter_iterations_b_r} pasos')
+    
+    else:
+        print('\nEl objetivo no se encuentra en las dos listas\nReintentelo de nuevo')
+        target = int(input('\nQue numero quieres encontrar?\n'))
+        counter_iterations = 0
+        found_l, counter_iterations_l = busqueda_lineal(list_l, target)
+        found_b_r, counter_iterations_b_r = busqueda_binaria_r(list_b_r, 0, len(list_b_r), target, counter_iterations)
+
+        print(f'{list_l}\n{list_b_r}')
+        
+        print(f'\nUsando Busqueda Lineal\nEl elemento {target} {"esta" if found_l else "no esta"} en la lista')
+        print(f'Se realizaron {counter_iterations_l} pasos')
+
+        print(f'\nUsando Busqueda Binaria Recursiva\nEl elemento {target} {"esta" if found_b_r else "no esta"} en la lista')
+        print(f'Se realizaron {counter_iterations_b_r} pasos')
     
